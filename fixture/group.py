4221a262ab9ad4fb3c72523ee4ba0794dab7190d
JW <jaanewhite@gmail.com>
2020-06-13 12:38:07 +0300

    Добавлены тесты инзменеия групп и удаления контактов

--- fixture/group.py
+++ fixture/group.py
@@ -37,3 +37,21 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
+
+    def edit_first_group(self,group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # click edit button
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
