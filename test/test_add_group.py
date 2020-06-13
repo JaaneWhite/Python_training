0e544debf0df6214629da42cf010e10f2e6720d9
JW <jaanewhite@gmail.com>
2020-06-13 09:33:38 +0300

    Выделен вспомогательный класс GroupHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,10 +12,10 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="NameG", header="GLead", footer="bla bla bla"))
+    app.group.create(Group(name="NameG", header="GLead", footer="bla bla bla"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
